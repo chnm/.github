82adde92ec7ffca61ef4aedbb1396f7cbf47b1cc
qtrinh2 <qtrinh2@gmu.edu>
2024-01-30 14:30:36 -0500

    Update and rename validate_input.sh to validate_inputs.sh

--- .github/scripts/validate_inputs.sh
+++ .github/scripts/validate_inputs.sh
@@ -2,6 +2,7 @@
 
 # this shell script will validate inputs used in the reusable workflows located
 # under .github/workflows/* and expects the inputs to be passed via environment variables.
+# i.e. hacky way to restrict input values
 
 # validate website-fqdn input used in hugo build-release-deploy workflow
 if [ -n "$WEBSITE_FQDN" ]; then
