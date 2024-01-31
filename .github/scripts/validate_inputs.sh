d4c010c6788d97f61f059b7de4e3bb943ff3ed05
qtrinh2 <qtrinh2@gmu.edu>
2024-01-31 08:06:39 -0500

    Merge hugo-workflow into main (#2)
    * added hugo--build-release-deploy.yml
    * added validate_inputs.sh

---
+++ .github/scripts/validate_inputs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# this shell script will validate inputs used in the reusable workflows located
+# under .github/workflows/* and expects the inputs to be passed via environment variables.
+# i.e. hacky way to restrict input values
+
+# validate website-fqdn input used in hugo build-release-deploy workflow
+if [ -n "$WEBSITE_FQDN" ]; then
+  case "$WEBSITE_FQDN" in
+    "deathbynumbers.org"|\
+    "mathhumanists.org"|\
+    "test1.rrchnm.org")
+        exit 0
+    ;;
+    *)
+        exit 1
+    ;;
+  esac
+fi
+
+exit 1
+
