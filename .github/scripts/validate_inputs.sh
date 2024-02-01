6d4d9ccf6408103d56b61056daa4b51f7c59b13b
qtrinh2 <qtrinh2@gmu.edu>
2024-02-01 13:22:59 -0500

    populating validate_inputs.sh with more website FQDNs

--- .github/scripts/validate_inputs.sh
+++ .github/scripts/validate_inputs.sh
@@ -7,7 +7,11 @@
 # validate website-fqdn input used in hugo build-release-deploy workflow
 if [ -n "$WEBSITE_FQDN" ]; then
   case "$WEBSITE_FQDN" in
+    "connectingthreads.co.uk"|\
+    "dev.connectingthreads.co.uk"|\
     "deathbynumbers.org"|\
+    "dev.deathbynumbers.org"|\
+    "earlymodernviolence.org"|\
     "dev.earlymodernviolence.org"|\
     "mathhumanists.org"|\
     "test1.rrchnm.org")
